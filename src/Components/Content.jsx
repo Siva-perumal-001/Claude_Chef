2d419c570ef758df70844b7aee4d0eabe88628ff
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-08-03 22:21:28 +0530

    Apply Conditional rendering to the page. whenever user add the recipe it will show the ingredient section.

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -9,26 +9,37 @@ function Content() {
         return <li className='list-item' key={item}>{item}</li>
     })
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient");
         setIngredients(prevIngredient => [...prevIngredient, newIngredient]);
     }
 
+    let length = ingredients.length;
+    console.log(length);
+
     return (
         <>
-            <form className='btm' onSubmit={handleSubmit}>
+            <form className='btm' action={addIngredient}>
                 <div className='bar'>
                     <input className='search' type="text" name="ingredient" placeholder='e.g Chicken' autoComplete='off' />
                     <button>+ Add Ingredient</button>
                 </div>
             </form>
-            <div className='ingredient'> 
-                <ul className='ing-list'>
-                    {ingredientList}
-                </ul>
-            </div>
+            {length > 0 &&
+                <section className='ingredient-sec'>
+                    <h1>Ingredients on hand :</h1>
+                    <ul className='ing-list'>
+                        {ingredientList}
+                    </ul>
+                    <div className='generate-recipe'>
+                        <div className='generate-recipe-text'>
+                            <h3>Ready for a recipe?</h3>
+                            <p>Generate a recipe from your list of ingredients</p>
+                        </div>
+                        <button className='generate-recipe-btn'>Get a recipe</button>
+                    </div>
+                </section>
+            }
         </>
     )
 }
